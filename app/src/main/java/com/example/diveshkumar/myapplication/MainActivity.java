45dd784fd79267981cd354fd666c9f00879ed891
kdivesh4 <kdivesh4@gmail.com>
2017-11-13 20:52:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/diveshkumar/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.diveshkumar.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+Button button;
+TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView =(TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(i);
+
+            }
+        });
+            }
+}
